ea6b2fe4a7480310466f17420e177d793920200d
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-27 15:22:07 -0500

    finished drumkit project

--- 1-drumkit/script.js
+++ 1-drumkit/script.js
@@ -1,8 +1,19 @@
-window.addEventListener('keydown', function(ele){
-    const audio = document.querySelector(`audio[data-key="${ele.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${ele.keyCode}"]`);
-    if(!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
-});
+ function playSound(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        if (!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('playing');
+    }
+
+    function removeTransition(e) {
+        if (e.propertyName !== "transform") return;
+        this.classList.remove("playing");
+    }
+
+    const keys = document.querySelectorAll(".key");
+    keys.forEach(key =>
+        key.addEventListener("transitionend", removeTransition));
+
+    window.addEventListener("keydown", playSound);
